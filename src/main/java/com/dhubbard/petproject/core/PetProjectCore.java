e4dcb1809c07e65b0678edcbb9e35a51aeff4531
DavisHubbard <davishubbard1@gmail.com>
2020-08-20 20:01:20 -0400

    Change the package

--- src/main/java/com/dhubbard/petproject/core/PetProjectCore.java
+++ src/main/java/com/dhubbard/petproject/core/PetProjectCore.java
@@ -1,12 +1,12 @@
-package com.in28minutes.rest.webservices.restfulwebservices;
+package com.dhubbard.petproject.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestfulWebServicesApplication {
+public class PetProjectCore {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServicesApplication.class, args);
+		SpringApplication.run(PetProjectCore.class, args);
 	}
 }
